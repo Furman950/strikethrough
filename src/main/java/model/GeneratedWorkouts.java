a0f6ff203ab4f2c67f008112d2d84d3469adf074
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-03-01 09:53:13 -0700

    qwertyuio

---
+++ src/main/java/model/GeneratedWorkouts.java
@@ -0,0 +1,19 @@
+package model;
+
+public enum GeneratedWorkouts
+{
+    PUSH_UPS,
+    PULL_UPS,
+    SIT_UPS,
+    SQUATS,
+    TRICEP_DIPS,
+    BURPEES,
+    PLANK,
+    WALL_SITS,
+    JUMPING_JACKS,
+    HANDSTAND_PUSH_UPS,
+    MOUNTAIN_CLIMBERS,
+    LUNGES,
+    BICEP_CURLS,
+    BENCH_PRESS;
+}
